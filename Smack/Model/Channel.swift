3dc07b5defcd010a97b1e28de183d6c0aaf990e6
Dylan <dylan@starkiller.local>
2017-12-09 15:39:11 -0800

    Lesson 19

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Dylan on 12/9/17.
+//  Copyright © 2017 Dylan. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
